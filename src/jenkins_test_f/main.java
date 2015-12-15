20f5b7f7404fb756521e59fb1167dcfc615b9331
chendamon <wangchendamon@gmail.com>
2015-12-15 15:36:27 +0800

    test

--- src/jenkins_test_f/main.java
+++ src/jenkins_test_f/main.java
@@ -1,10 +1,13 @@
 package jenkins_test_f;
 
-public class main 
+
+public class Main 
 {
 	public static void main(String[] args)
 	{
 		System.out.println("hello world!");
+		//一处修改的test
+		System.out.println("I have changed, fool!");
 	}
 
 }
